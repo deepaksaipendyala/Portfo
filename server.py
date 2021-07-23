0a9dbf68fdc5f6f00eca37665dee503cfeac3b6c
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2021-07-23 19:26:29 +0000

    Fixed bug and added content

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# @author deepak sai pendyala
 import csv
 from flask import Flask,render_template,url_for,redirect,request
 app = Flask(__name__)
@@ -11,14 +12,14 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_database(data):
-    with open('database.txt',mode='a')as database:
+    with open('./Portfo/database.txt',mode='a')as database:
         name=data["name"]
         email=data["email"]
         message=data['message']
         file=database.write(f'\n {name},{email},{message}')
 
 def write_to_csv(data):
-    with open('database.csv',mode='a',newline='')as database2:
+    with open('./Portfo/database.csv',mode='a',newline='')as database2:
         name=data["name"]
         email=data["email"]
         message=data['message']
